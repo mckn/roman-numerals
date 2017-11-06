b6a496af389ab5882f9c80ce5712b130c295c93a
Marcus Andersson <systemvetaren@gmail.com>
2017-11-06 19:05:28 +0100

    feels like it is working for hundreds as well.

--- romanize.js
+++ romanize.js
@@ -2,12 +2,18 @@
 
 const base = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 const tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+const hundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
 
 module.exports = number => {
   let converted = "";
 
-  if (number > 10) {
-    converted += tens[Math.floor(number / 10, 0) - 1];
+  if (number > 99) {
+    converted += hundreds[Math.floor(number / 100) - 1];
+    number = number % 100;
+  }
+
+  if (number > 9) {
+    converted += tens[Math.floor(number / 10) - 1];
     number = number % 10;
   }
 
