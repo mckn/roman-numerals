5d43944dd6c3c9e362b5acd20c8ab7c4092a53f5
Marcus Andersson <systemvetaren@gmail.com>
2017-11-06 23:19:33 +0100

    added one more test.

--- test/api/get_roman_numerals.js
+++ test/api/get_roman_numerals.js
@@ -15,6 +15,18 @@ test("GET /v1/roman/50 should return 200 OK with response numeral L", async t =>
   t.is(res.body.roman, "L");
 });
 
+test("GET /v1/roman/10 should return 200 OK with response numeral X", async t => {
+  t.plan(4);
+
+  const url = "/v1/roman/10";
+  const res = await request(server).get(url);
+
+  t.is(res.status, 200);
+  t.is(res.body.href, url);
+  t.is(res.body.number, 10);
+  t.is(res.body.roman, "X");
+});
+
 test("GET /v1/roman/100000 should return 404 missing", async t => {
   const url = "/v1/roman/100000";
   const res = await request(server).get(url);
