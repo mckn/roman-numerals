4b19a2b1f03d70c5b4d313f8b8f4d4b668e783b8
Marcus Andersson <systemvetaren@gmail.com>
2017-11-06 18:55:11 +0100

    added tests for two digits (draft implementation).

--- romanize.js
+++ romanize.js
@@ -1,18 +1,20 @@
 "use strict";
 
-const map = {
-  "0": "",
-  "1": "I",
-  "2": "II",
-  "3": "III",
-  "4": "IV",
-  "5": "V",
-  "6": "VI",
-  "7": "VII",
-  "8": "VIII",
-  "9": "IX"
-};
+const base = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+const tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 
 module.exports = number => {
-  return map[String(number)];
+  let converted = "";
+
+  if (number > 10) {
+    converted += tens[Math.floor(number / 10, 0) - 1];
+    number = number % 10;
+  }
+
+  if (number <= 0) {
+    return converted;
+  }
+
+  converted += base[number - 1];
+  return converted;
 };
---
+++ test/romanize_two_digits.js
@@ -0,0 +1,38 @@
+const test = require("ava");
+const romanize = require("../romanize");
+
+test("romanize should convert 11 to XI", t => {
+  t.is(romanize(11), "XI");
+});
+
+test("romanize should convert 12 to XII", t => {
+  t.is(romanize(12), "XII");
+});
+
+test("romanize should convert 14 to XIV", t => {
+  t.is(romanize(14), "XIV");
+});
+
+test("romanize should convert 15 to XV", t => {
+  t.is(romanize(15), "XV");
+});
+
+test("romanize should convert 16 to XVI", t => {
+  t.is(romanize(16), "XVI");
+});
+
+test("romanize should convert 19 to XIX", t => {
+  t.is(romanize(19), "XIX");
+});
+
+test("romanize should convert 20 to XX", t => {
+  t.is(romanize(20), "XX");
+});
+
+test("romanize should convert 25 to XXV", t => {
+  t.is(romanize(25), "XXV");
+});
+
+test("romanize should convert 34 to XXXIV", t => {
+  t.is(romanize(34), "XXXIV");
+});
